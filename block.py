533916cf4641a43e237037e2993a952ca717476b
Vlad Hategan <vhateg@gmail.com>
2024-12-16 02:11:57 +0100

    Latest mods

--- block.py
+++ block.py
@@ -1,12 +1,25 @@
 import numpy as np
 
 class Block:
-    def __init__ (self, bid, present, x, y, angle):
+    def __init__ (self, bid, present, x, y, angle, enabled=False):
         self.bid = bid
         self.present = present
         self.x = x
         self.y = y
         self.angle = angle
+        self.enabled = enabled
+
+    def getID(self):
+        return self.bid
+    
+    def enable(self):
+        self.enabled = True
+
+    def disable(self):
+        self.enabled = False
+
+    def getEnabled(self):
+        return self.enabled
     
     def setLocation(self, x, y, angle):
         self.x = x
@@ -16,40 +29,57 @@ class Block:
     def setPresent(self, present):
         self.present = present
 
+    def getPresent(self):
+        return self.present
+
     def getRect(self):
         return (self.x, self.y, 100, 100)
     
     def getColor(self):
-        if (self.id < 10):
+        if (self.bid < 10):
             return (255, 0, 0)
-        if (self.id >= 10 and self.id < 20):
+        if (self.bid >= 10 and self.bid < 20):
             return (0, 255, 0)
-        if (self.id >= 20):
+        if (self.bid >= 20):
             return (0, 0, 255)
     
-    def slider_to_frequency(self, slider_position, f_min=20, f_max=20000, slider_range=600):
+    def __slider_to_frequency(self, slider_position, f_min=20, f_max=20000, slider_range=600):
         octaves = np.log2(f_max / f_min)
         r = slider_range / octaves
         return f_min * (2 ** (slider_position / r))
+    
+    def getDuration(self, bpm, fps):
+        return self.x
     
     def getWave(self):
         
         sample_rate = 44100
         amplitude = 0.5
-        duration = self.bid
-        frequency = self.slider_to_frequency(self.y)
+        duration = self.getDuration
+        frequency = self.__slider_to_frequency(self.y)
 
         # generate sine wave
-        if (id < 10):
+        if (self.bid < 10):
             t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
             wave = amplitude * np.sin(2 * np.pi * frequency * t)
         # generate sawtooth wave
-        if (id >= 10 and id < 20):
+        if (self.bid >= 10 and self.bid < 20):
             t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
             wave = amplitude * 2 * (t * frequency % 1) - amplitude
         # generate square wave
-        if (id >= 20):
+        if (self.bid >= 20):
             t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
             wave = amplitude * np.sign(np.sin(2 * np.pi * frequency * t))
 
         return wave
+
+        def play(self):
+            #play sound using pyaudio in separate thread
+            stream = audio.open(format=pyaudio.paFloat32,
+                                channels=1,
+                                rate=44100,
+                                output=True)
+            stream.write(self.getWave().astype(np.float32).tobytes())
+            stream.stop_stream()
+            stream.close()
+            self.disable()
--- main.py
+++ main.py
@@ -2,97 +2,125 @@ import cv2
 import numpy as np
 import pygame
 import pyaudio
+from block import Block
+
+class Main:
+
+    def main():
+
+        pygame.init()
+        clock = pygame.time.Clock()
+        fps = 60
+        bpm = 120
+        sweep = 0
+
+        audio = pyaudio.PyAudio()
+
+        capture = cv2.VideoCapture(0)
+        canvas = pygame.display.set_mode((1024, 600))
+
+        width = capture.get(cv2.CAP_PROP_FRAME_WIDTH )
+        height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT )
+        print("Size: ", width, height)
+
+        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        parameters = cv2.aruco.DetectorParameters()
+        detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
+        pygame.display.set_caption("BeatSketch") 
+        exit = False
+        
+        try:
+            while not exit:
+                for event in pygame.event.get(): 
+                    if event.type == pygame.QUIT: 
+                        exit = True
+
+                sweep += 1024/((60/bpm)*fps);
+                if sweep >= 1024:
+                    sweep = 0
+                
+                ret, frame = capture.read()
+                if not ret:
+                    print("error capturing frame")
+                    break
+                grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                corners, ids, _ = detector.detectMarkers(grayscale)
+                blocks = []
+                for i in range(0, 30):
+                    block = Block(i, False, 0, 0, 0)
+                    blocks.append(block)
+
+                if ids is not None:
+                    ids = ids.flatten()
+
+                    for marker_corners, marker_id in zip(corners, ids):
+                        corners = marker_corners.reshape((4, 2))
+                        (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+                        topLeft = (int(topLeft[0]), int(topLeft[1]))
+                        topRight = (int(topRight[0]), int(topRight[1]))
+                        bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                        bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+
+                        cX = int(1024-((topLeft[0] + bottomRight[0])*1024/width/2))
+                        cY = int((topLeft[1] + bottomRight[1])*600/height/2)
+
+                        vector = np.array(topRight) - np.array(topLeft)
+                        angle = np.arctan2(vector[1], vector[0])
+
+                        angle_degrees = np.degrees(angle)
+                        if angle_degrees < 0:
+                            angle_degrees += 360
+
+                        print("Marker ID: ", marker_id)
+                        print(blocks)
+                        for block in blocks:
+                            print(f"Block ID: {block.getID()}")
+                            if np.int32(block.getID()) == np.int32(marker_id):
+                                block.setLocation(cX, cY, angle_degrees)
+                                block.setPresent(True)
+                                print("BLOCKus")
+                                if sweep > cX and sweep < cX+power:
+                                    block.enable()
+
+                        print(f"Marker ID: {marker_id}")
+                        print(f"Location: X={cX}, Y={cY}")
+                        print(f"Z Rotation: {angle_degrees:.2f} degrees")
+                        print('---')
+                        power = 1024*(2**(angle_degrees*4/360))/16
+                            
+                pygame.draw.rect(canvas, (255, 255, 255), (sweep, 0, 2, 600))
+                for block in blocks:
+                    if block.getPresent():
+                        pygame.draw.rect(canvas, block.getColor(), block.getRect())
+                        print("BLOCK")
+                        # play sound using pyaudio
+                        if block.getEnabled():
+                            stream = audio.open(format=pyaudio.paFloat32,
+                                                channels=1,
+                                                rate=44100,
+                                                output=True)
+                            stream.write(block.getWave().astype(np.float32).tobytes())
+                            stream.stop_stream()
+                            stream.close()
+                            block.disable()
 
 
-def main():
+                font = pygame.font.SysFont(None, 24)
+                bpm_text = "BPM: " + str(bpm)
+                img = font.render(bpm_text, True, (125,125,125))
+                canvas.blit(img, (20, 20))
 
-    pygame.init()
-    clock = pygame.time.Clock()
-    fps = 87
-    bpm = 120
-    sweep = 0
+                pygame.display.update()
+                #print(sweep)
+                canvas.fill((0, 0, 0))
+                blocks.clear()
+                clock.tick(fps)
 
-    capture = cv2.VideoCapture(0)
-    canvas = pygame.display.set_mode((1024, 600))
+        except KeyboardInterrupt:
+            pass
 
-    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH )
-    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT )
-    print("Size: ", width, height)
-
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-    parameters = cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-
-    pygame.display.set_caption("My Board") 
-    exit = False
-
-    blocks = []
-
-    try:
-        while not exit:
-            for event in pygame.event.get(): 
-                if event.type == pygame.QUIT: 
-                    exit = True
-
-            sweep += 1024/((60/bpm)*fps);
-            if sweep >= 1024:
-                sweep = 0
-            
-            ret, frame = capture.read()
-            if not ret:
-                print("error capturing frame")
-                break
-            grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            corners, ids, _ = detector.detectMarkers(grayscale)
-
-            if ids is not None:
-                ids = ids.flatten()
-
-                for marker_corners, marker_id in zip(corners, ids):
-                    corners = marker_corners.reshape((4, 2))
-                    (topLeft, topRight, bottomRight, bottomLeft) = corners
-
-                    topLeft = (int(topLeft[0]), int(topLeft[1]))
-                    topRight = (int(topRight[0]), int(topRight[1]))
-                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-
-                    cX = int(1024-((topLeft[0] + bottomRight[0])*1024/width/2))
-                    cY = int((topLeft[1] + bottomRight[1])*600/height/2)
-
-                    vector = np.array(topRight) - np.array(topLeft)
-                    angle = np.arctan2(vector[1], vector[0])
-
-                    angle_degrees = np.degrees(angle)
-                    if angle_degrees < 0:
-                        angle_degrees += 360
-
-                    print(f"Marker ID: {marker_id}")
-                    print(f"Location: X={cX}, Y={cY}")
-                    print(f"Z Rotation: {angle_degrees:.2f} degrees")
-                    print('---')
-                    power = 1024*(2**(angle_degrees*4/360))/16
-
-                    enabled = False
-                    if sweep > cX and sweep < cX+power:
-                        enabled = True
-
-                    blocks.append((cX, cY, angle_degrees, enabled))
-            pygame.draw.rect(canvas, (255, 255, 255), (sweep, 0, 2, 600))
-            for block in blocks:
-                if not block[3]:
-                    pygame.draw.rect(canvas, (255, 0, 0), (block[0], block[1], power, 50))
-                else:
-                    pygame.draw.rect(canvas, (0, 255, 0), (block[0], block[1], power, 50))
-            pygame.display.update()
-            print(sweep)
-            canvas.fill((0, 0, 0))
-            blocks.clear()
-            clock.tick(fps)
-
-    except KeyboardInterrupt:
-        pass
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
